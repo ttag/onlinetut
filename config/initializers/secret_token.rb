0b6fc5a525e98081efe5b9c514ce0e16bb54c12f
x.eshan@hotmail.com <x.eshan@hotmail.com>
2011-01-04 00:15:39 +0500

    fresh arch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinetut::Application.config.secret_token = '981f92b2f4e04aca375bfcca8e6b3df07fa0b5495ffe2d9c6a628cdc1c5b455ea4da85b8ac4319e2edf01a40d6b9bff207b923c0ce064b181cea70ffb2a0739a'
